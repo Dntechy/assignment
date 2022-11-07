718dc452b82b7f96767b42e2d0cc27ef312c3caf
Dntechy <danielnjoroge593.dn@gmail.com>
2022-11-07 16:03:32 +0300

    Update Q4_Request.py

--- Q4_Request.py
+++ Q4_Request.py
@@ -1,15 +1,37 @@
-# importing the requests library
+
 import requests
 
-# api-endpoint
 BASE_URL = 'https://fakestoreapi.com'
-
-# query parameter
-query_params = {
-    "limit": 3
+#Randomize this data
+new_product = {
+    "title": 'test product',
+    "price": 13.5,
+    "description": 'lorem ipsum set',
+    "image": 'https://i.pravatar.cc',
+    "category": 'electronic'
 }
+#Send a Post requesst
+response = requests.post(f"{BASE_URL}/products", json=new_product)
+_response = response.json()
+#breakpoint()
+#Not a must to print
+#print(response.json())
 
-response = requests.get(f"{BASE_URL}/products", params=query_params)
-print(response.json())
-print(response.status_code)
+#Set variables from the response
+product_id = _response["id"]
+product_title = _response["title"]
 
+#Now we go to the browser
+#First pip install selenium
+
+from selenium import webdriver
+
+#driver = webdriver.Chrome('./chromedriver')
+#driver = webdriver.Chrome(executable_path="C:\\browserdrivers\\chromedriver.exe")
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(BASE_URL + "/products/" + str(product_id))
+#search_bar = driver.find_element_by_xpath(//pre[contains(text(),'test product')])
+breakpoint()
